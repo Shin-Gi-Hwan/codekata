e4df380d9d0aaf4937f9d8f2c7784e8a800e96da
gihwan.Shin <73213158+Shin-Gi-Hwan@users.noreply.github.com>
2025-02-20 08:13:41 +0900

    [level 1] Title: 내적, Time: 0.05 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,13 +1,9 @@
 class Solution {
  public int solution(int[] a, int[] b) {
         int answer = 0;
-        int[] result = new int[a.length];
         for (int i = 0; i < a.length; i++) {
-            result[i] = a[i] * b[i];
-        }
-
-        for (int i = 0; i < result.length; i++) {
-            answer += result[i];
+            int result = a[i] * b[i];
+            answer += result;
         }
 
         return answer;
