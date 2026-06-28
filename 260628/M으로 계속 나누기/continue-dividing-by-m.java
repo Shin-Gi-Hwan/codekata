2f4fc0373aed08ddc81d936075b834511913aa6c
gihwan.Shin <73213158+Shin-Gi-Hwan@users.noreply.github.com>
2026-06-28 20:51:03 +0900

    [Passed] M으로 계속 나누기 (149ms, 10MB)

---
+++ 260628/M으로 계속 나누기/continue-dividing-by-m.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int result = N;
+        
+        while(result > 0) {
+            System.out.println(result);
+            result = result / M;
+        }
+    }
+}
